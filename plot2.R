c7c3ae220b057ac7f8c0bdb935d271a5e7ede7f2
Mani Yangkatisal <ymani2435@yahoo.com>
2015-08-09 21:50:22 +0800

    Course Project 1

---
+++ plot2.R
@@ -0,0 +1,21 @@
+## Getting full dataset from CSV file
+data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+##Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
